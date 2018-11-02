e9e5484d0393fb65c3fec4e8468455c9dd23d961
Tyler Krys <tylerkrys@gmail.com>
2018-11-01 20:26:43 -0700

    Refactor intFactorial() to simplify logic

--- integer-factorial.js
+++ integer-factorial.js
@@ -1,13 +1,11 @@
 
 // Given a non-negative integer, returns the factorial
 function intFactorial(int) {
-  let number
-
   if (int < 0) {
     return NaN
-  } else {
-    number = 1
   }
+
+  let number = 1
 
   for (let i = 1; i <= int; i++) {
     number *= i
